13967c63a500136d075181cfe52c2548e47311b2
Felix <barosiofelix@gmail.com>
2022-08-25 23:50:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.length <= 1){
+    return true;
+  }else if(word[0] == word[word.length - 1]){
+    return isPalindrome(word[1, word.length - 1])
+  }
+  return false;
 }
 
 /* 
   Add your pseudocode here
+  Add your pseudocode here
+  add and check size of the string
+  if 1st and last character are same
+  remove elements
 */
 
 /*
   Add written explanation of your solution here
+  Add written explanation of your solution here
+  line 3 and 5 are same condition when we have string same length
+  less than or equal to 
 */
 
 // You can run `node index.js` to view these console logs
